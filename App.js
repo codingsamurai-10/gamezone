547cc05d03f9acb762060b5a63cdc6e2ef8930d4
Aditya Rana <official.adityarana01@gmail.com>
2021-09-16 05:54:37 +0530

    Added custom font styles

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Home from './screens/Home';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  })
+
   return (
     <Home />
   );
