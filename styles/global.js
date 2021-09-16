775312ec7f2f029008d2f6ec321bcb85b6137c0f
Aditya Rana <official.adityarana01@gmail.com>
2021-09-16 05:59:35 +0530

    Separated out global styles

---
+++ styles/global.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const globalStyles = StyleSheet.create({
+    container: {
+        padding: 24,
+        flex: 1
+    },
+    titleText: {
+        fontFamily: 'nunito-bold',
+        fontSize: 18,
+        color: '#333'
+    },
+    paragraph: {
+        marginVertical: 8,
+        lineHeight: 20
+    }
+})
+
+export default globalStyles;
