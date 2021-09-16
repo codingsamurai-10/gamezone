bf7daea78b6eac83cfd29e9011deaf31cfceb25f
Aditya Rana <official.adityarana01@gmail.com>
2021-09-16 06:52:11 +0530

    Create stack navigator

---
+++ routes/HomeStack.js
@@ -0,0 +1,20 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/Home";
+import ReviewDetails from "../screens/ReviewDetails";
+import React from "react";
+
+const Stack = createNativeStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ReviewDetails" component={ReviewDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default HomeStack;
