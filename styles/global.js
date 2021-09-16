02d1a98146845b732d6a41b6eeb15ae00f068e39
Aditya Rana <official.adityarana01@gmail.com>
2021-09-16 06:37:01 +0530

    Changed font to the installedo ne

--- styles/global.js
+++ styles/global.js
@@ -1,19 +1,19 @@
 import { StyleSheet } from "react-native";
 
 const globalStyles = StyleSheet.create({
-    container: {
-        padding: 24,
-        flex: 1
-    },
-    titleText: {
-        fontFamily: 'nunito-bold',
-        fontSize: 18,
-        color: '#333'
-    },
-    paragraph: {
-        marginVertical: 8,
-        lineHeight: 20
-    }
-})
+  container: {
+    padding: 24,
+    flex: 1,
+  },
+  titleText: {
+    fontFamily: "Inter_900Black",
+    fontSize: 18,
+    color: "#333",
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+});
 
 export default globalStyles;
