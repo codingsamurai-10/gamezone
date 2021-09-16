c85ca7dca2fb7755c14a120521c0c078a13b4d7c
Aditya Rana <official.adityarana01@gmail.com>
2021-09-16 06:52:30 +0530

    Rendered stacknavigator instead of home screen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Home from "./screens/Home";
 import AppLoading from "expo-app-loading";
 import {
   useFonts,
   Nunito_700Bold,
   Nunito_400Regular,
 } from "@expo-google-fonts/nunito";
+import HomeStack from "./routes/HomeStack";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -16,6 +16,6 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
-    return <Home />;
+    return <HomeStack />;
   }
 }
